4be34c3cb9fd877da1b7919820028d5fd819fd60
Deepak R <112789260+DK-DataHub@users.noreply.github.com>
2025-10-30 10:18:00 +0530

    Add files via upload

---
+++ example07.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number (a):"))
+b = int(input("Enter the second number (b):"))
+#Swapping conditions
+temp = a
+a = b
+b = temp
+print(f"The values after swapping a & b is: a = {a}, b = {b}")
---
+++ example09.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number a:"))
+b = int(input("Enter the number b:"))
+if (a % 2 == 0):
+    print("The number is Even")
+else:
+    print("The number is Odd")
+if (b % 2 == 0):
+    print("The number is Even")
+else:
+    print("The number is Odd")
