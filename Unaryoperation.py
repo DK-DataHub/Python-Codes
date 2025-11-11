45ebfec9a1457c2626d8ddf681c95df180abd9cd
Deepak R <112789260+DK-DataHub@users.noreply.github.com>
2025-11-11 10:59:49 +0530

    Implement unary operations on NumPy arrays

---
+++ Unaryoperation.py
@@ -0,0 +1,12 @@
+import numpy as np
+a = np.array([[1, 2, 3,
+               4, 5, 6]])
+b = np.array([[7, 8, 9,
+               10, 11, 12]])
+#Adding 1 to a
+print("\nAddition of 1 to each elements of a:", a + 1)
+#Subracting 2 from b
+print("\nSubraction of 2 from each elements of b:", b - 2)
+#Performing Unary Operation
+print("\nSum of all elements in array a:\n",a.sum())
+print("\nSum of all elements in array b:\n",b.sum())
