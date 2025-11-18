9064485c19386b60464f99fdb393c6b72fde375a
Deepak R <112789260+DK-DataHub@users.noreply.github.com>
2025-11-18 18:32:54 +0530

    Create Dimension-num.py

---
+++ Dimension-num.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array(56)
+b = np.array([1, 2, 4])
+c = np.array([[1, 2, 3] , [4, 5, 6]])
+d = np.array([[[1, 2, 3] , [4, 5, 6] , [7, 8, 9] , [10, 11, 12]]])
+print(a.ndim,"D")
+print(b.ndim,"D")
+print(c.ndim,"D")
+print(d.ndim,"D")
