f355ad32534f01f4c57638345e7a01b4fd3ac7e2
Deepak R <112789260+DK-DataHub@users.noreply.github.com>
2025-12-01 10:29:54 +0530

    Expand Rankholders data with additional entries

--- Pandas-Dataframe.py
+++ Pandas-Dataframe.py
@@ -1,8 +1,8 @@
 import pandas as pd
 Rankholders = {
-    'Name': ["Deepak", "Karthick", "Vinoth"],
-    'Top_Subjects': ["Physics", "Maths", "Engineering Graphics"],
-    'CGPA': [9, 8, 7]
+    'Name': ["Deepak", "Karthick", "Vinoth", "Ganesh", "Faruk", "Vignesh", "Harish", "Kanagavel"],
+    'Top_Subjects': ["Physics", "Maths", "Engineering Graphics", "Chemistry", "Geography", "UHV", "Python", "Java"],
+    'CGPA': [9, 8, 7, 8, 9, 8, 7, 7]
 }
 Rank = pd.DataFrame(Rankholders)
-print(Rank)
+print(Rank.loc[[0, 2, 4, 6, 7]])
